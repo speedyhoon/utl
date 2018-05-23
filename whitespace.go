23584652592cf47dd085089c3c3d5801c8a6cbab
Cam <eventbucket@hotmail.com>
2018-05-23 14:08:15 +1000

    Add quotes & whitespace

---
+++ whitespace.go
@@ -0,0 +1,28 @@
+package utl
+
+import (
+	"strings"
+	"unicode"
+)
+
+//StripWhiteSpace removes unicode whitespace characters and condenses condenses multiple spaces to a single character
+func StripWhiteSpace(str string) string {
+	a := strings.Map(func(r rune) rune {
+		//credit: Tim Cooper //stackoverflow.com/questions/32081808/strip-all-whitespace-from-a-string
+		if unicode.IsSpace(r) && r != ' ' {
+			return -1
+		}
+		return r
+	}, str)
+
+	l := len(a)
+	for {
+		//Replace double spaces with a single space
+		a = strings.Replace(a, "  ", " ", -1)
+		if l == len(a) {
+			break
+		}
+		l = len(a)
+	}
+	return a
+}
