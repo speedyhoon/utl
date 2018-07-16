d1e40b9c8f7d57283a0570095bd5b8ae27a27234
Cam <eventbucket@hotmail.com>
2018-07-16 15:12:06 +1000

    rename var

--- dir.go
+++ dir.go
@@ -7,9 +7,9 @@ import (
 
 //Cwd returns the current working directory where the application is stored.
 func Cwd() string {
-	dirRun, err := os.Executable()
+	path, err := os.Executable()
 	if err != nil {
 		return "."
 	}
-	return filepath.Dir(dirRun)
+	return filepath.Dir(path)
 }
