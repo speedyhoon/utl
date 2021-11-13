477885b871b12f01eeecbe0c08c89a663c0ba769
sh <16303791+speedyhoon@users.noreply.github.com>
2021-11-13 16:38:05 +1100

    Improve Cwd().

--- dir.go
+++ dir.go
@@ -7,7 +7,7 @@ import (
 
 //Cwd returns the current working directory where the application is stored.
 func Cwd() string {
-	path, err := os.Executable()
+	path, err := os.Getwd()
 	if err != nil {
 		return "."
 	}
