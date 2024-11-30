3e31b3758160da896a47d40fb995f7b64c7ceec0
sh <16303791+speedyhoon@users.noreply.github.com>
2024-11-30 13:30:41 +1100

    Don't panic when nil is passed to NameOfFunc().

--- reflect.go
+++ reflect.go
@@ -6,8 +6,12 @@ import (
 	"strings"
 )
 
-// NameOfFunc returns the function name as a string.
+// NameOfFunc uses reflection to return the function name as a string.
 func NameOfFunc(function any) (name string) {
+	if function == nil {
+		return
+	}
+
 	var ok bool
 	name, ok = function.(string)
 	if !ok {
