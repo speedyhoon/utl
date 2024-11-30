31f86caafecd1594c45c074ba61fdafa3e5af8b8
sh <16303791+speedyhoon@users.noreply.github.com>
2024-11-30 13:30:41 +1100

    Don't panic when nil is passed to NameOfFunc().

--- reflect_test.go
+++ reflect_test.go
@@ -2,6 +2,8 @@ package utl
 
 import (
 	"github.com/stretchr/testify/assert"
+	"net/http"
+	"strconv"
 	"testing"
 	"time"
 )
@@ -13,11 +15,18 @@ func TestNameOf(t *testing.T) {
 		want string
 	}
 	tests := []test{
+		{name: "", arg: nil},
 		{name: "empty", arg: "", want: ""},
 		{name: "string", arg: "zebra", want: "zebra"},
-		{name: "package", arg: "example.com/zoo/lion", want: "lion"},
+		{name: "package name", arg: "example.com/zoo/lion", want: "lion"},
 		{name: "time.Now", arg: time.Now, want: "time.Now"},
 		{name: "utl.Cwd", arg: Cwd, want: "utl.Cwd"},
+		{name: "utl.Exec", arg: Exec, want: "utl.Exec"},
+		{name: "utl.UtoA", arg: UtoA, want: "utl.UtoA"},
+		{name: "utl.Ordinal", arg: Ordinal, want: "utl.Ordinal"},
+		{name: "utl.LogBaseXUint", arg: LogBaseXUint, want: "utl.LogBaseXUint"},
+		{name: "strconv.FormatFloat", arg: strconv.FormatFloat, want: "strconv.FormatFloat"},
+		{name: "http.AllowQuerySemicolons", arg: http.AllowQuerySemicolons, want: "http.AllowQuerySemicolons"},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
