61965527b27f3bab3c43cb2df58f4e8201b90c4a
sh <16303791+speedyhoon@users.noreply.github.com>
2025-10-02 15:43:53 +1000

    Rename variable.

--- plural.go
+++ plural.go
@@ -17,29 +17,29 @@ func Plural(length int, single, multiple string) string {
 }
 
 // Ordinal gives you the input number in a rank/ordinal format. e.g. Ordinal(3, true) outputs "=3rd"
-func Ordinal(position uint, isEqual bool) (suffix string) {
+func Ordinal(position uint, isEqual bool) (ord string) {
 	if isEqual {
-		suffix = "="
+		ord = "="
 	}
 
-	suffix += strconv.FormatUint(uint64(position), 10)
+	ord += strconv.FormatUint(uint64(position), 10)
 
 	const first, second, third, eleventh, twelfth, thirteenth, ten, hundred = 1, 2, 3, 11, 12, 13, 10, 100
 
 	switch position % ten {
 	case first:
 		if position%hundred != eleventh {
-			return suffix + "st"
+			return ord + "st"
 		}
 	case second:
 		if position%hundred != twelfth {
-			return suffix + "nd"
+			return ord + "nd"
 		}
 	case third:
 		if position%hundred != thirteenth {
-			return suffix + "rd"
+			return ord + "rd"
 		}
 	}
 
-	return suffix + "th"
+	return ord + "th"
 }
