516ebf0aaecf21cbd2aadfd7583bc0c035e2ac0d
sh <16303791+speedyhoon@users.noreply.github.com>
2025-10-17 13:34:13 +1100

    Fix grammar.

--- brwsr/err.go
+++ brwsr/err.go
@@ -1,4 +1,4 @@
 // Package brwsr implements a way to open links in the user's default browser compatible with most operating systems.
 package brwsr
 
-const errMsg = "unable to open web browser for URL `%s`:\n%w"
+const errMsg = "unable to open a web browser for URL `%s`:\n%w"
